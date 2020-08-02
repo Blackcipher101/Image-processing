570636f059ea1ea1fc3a7be03b15fde34a26b551
Nehal Nevle <nehalsuperdude@gmail.com>
2020-08-02 23:37:35 +0530

    start

---
+++ video-capture.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(0)== ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
